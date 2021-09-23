e1af1da022c060566d4a2a76e3ed82dfd520eabf
Kevin Figueroa <kevin.figueroa@labo-ve.fr>
2021-09-23 13:40:21 +0200

    added user entity and login route

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SecurityController extends AbstractController {
+
+    /**
+     * @Route("/api/login", name="api_login", methods={"POST"})
+     */
+    public function login() {
+        $user = $this->getUser();
+        return $this->json([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+}
